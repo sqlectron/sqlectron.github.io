6fa3ccde13812212c8ff194bc7365fdebc26d487
Max Claus Nunes <maxcnunes@gmail.com>
2015-12-02 01:13:25 -0200

    Improve the meta tags

--- src/root.js
+++ src/root.js
@@ -16,6 +16,9 @@ if (typeof window !== 'undefined') {
 }
 
 
+const logo = require('./components/logo-512px.png');
+
+
 export default class Root extends React.Component {
   componentDidMount() {
     ga('create', 'UA-70854359-1', 'auto');
@@ -28,14 +31,22 @@ export default class Root extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <title>Sqlectron - A single DB client for all your relational DBs</title>
-          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <meta name="twitter:card" value="summary" />
-          <meta property="og:title" content="SQLECTRON" />
-          <meta property="og:type" content="article" />
-          <meta property="og:url" content="https://sqlectron.github.io" />
-          <meta property="og:image" content="https://camo.githubusercontent.com/5d0b5580048e574fa22f693f3ea9958dbe4bc027/68747470733a2f2f7777772e64726f70626f782e636f6d2f732f6474303674696634786f316b6862662f53514c454354524f4e2d34303070782e706e673f7261773d31" />
-          <meta property="og:description" content="A simple and lightweight SQL client desktop/terminal with cross database and platform support. " />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:title" content="Sqlectron - A single DB client for all your relational DBs"/>
+          <meta property="twitter:description" content="A simple and lightweight SQL client desktop/terminal with cross database and platform support."/>
+          <meta property="twitter:image:src" content={logo}/>
+          <meta property="twitter:image:width" content="512"/>
+          <meta property="twitter:image:height" content="176"/>
+          <meta property="og:url" content="https://sqlectron.github.io"/>
+          <meta property="og:site_name" content="Sqlectron"/>
+          <meta property="og:title" content="Sqlectron - A single DB client for all your relational DBs"/>
+          <meta property="og:description" content="A simple and lightweight SQL client desktop/terminal with cross database and platform support."/>
+          <meta property="og:image" content={logo}/>
+          <meta property="og:image:type" content="image/png"/>
+          <meta property="og:image:width" content="512"/>
+          <meta property="og:image:height" content="176"/>
           {!isDev && <link rel="stylesheet" type="text/css" href="/dist/style.css"/>}
         </head>
         <body>
