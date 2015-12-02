9c4d506e872cd64cba2c5d9e1c3a7502a6bdac06
Vinícius Krolow <krolow@gmail.com>
2015-12-02 00:19:55 -0200

    Add social meta tag for twitter/fb

--- src/root.js
+++ src/root.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ga from 'react-google-analytics';
+
 import Header from './components/header.jsx';
 import Hero from './components/hero.jsx';
 import AppInfoGUI from './components/app-info-gui.jsx';
@@ -28,6 +29,12 @@ export default class Root extends React.Component {
           <title>Sqlectron</title>
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <meta name="twitter:card" value="summary" />
+          <meta property="og:title" content="SQLECTRON" />
+          <meta property="og:type" content="article" />
+          <meta property="og:url" content="https://sqlectron.github.io" />
+          <meta property="og:image" content="https://camo.githubusercontent.com/5d0b5580048e574fa22f693f3ea9958dbe4bc027/68747470733a2f2f7777772e64726f70626f782e636f6d2f732f6474303674696634786f316b6862662f53514c454354524f4e2d34303070782e706e673f7261773d31" />
+          <meta property="og:description" content="A simple and lightweight SQL client desktop/terminal with cross database and platform support. " />
           {!isDev && <link rel="stylesheet" type="text/css" href="/dist/style.css"/>}
         </head>
         <body>
