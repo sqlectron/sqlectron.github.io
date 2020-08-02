3d535c88a11d6ec306ecd821ef5d545f69c673d8
Matthew Peveler <matt.peveler@gmail.com>
2020-08-02 13:07:47 -0400

    refactor website for webpack 4
    The transition from webpack 1 -> 4 allows a number of changes
    to how the site was being built previously. Minimally, this involved
    removal of a number of deprecated / removed plugins, upgrading
    the syntax, upgrading all the loaders, reconfiguring the loaders as
    made sense (e.g. babel activates react-hot-loader automatically).
    
    As part of this effort, the dist/ folder has also been removed, in-favor
    of instead using a dedicated gh-pages branch, which will get updated via
    a GH action workflow, instead of requiring a manual push on the part
    of a developer. This simplifies things, as well as makes "drive-by"
    commits easier.
    
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 import ReactDOMServer from 'react-dom/server';
 import Root from './root';
 
+/*
 export default function(path, props, render) {
   render('<!doctype html>' + ReactDOMServer.renderToString(<Root/>));
 }
@@ -9,3 +12,11 @@ export default function(path, props, render) {
 if (typeof document !== 'undefined') {
   React.render(<Root/>, document);
 }
+*/
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
