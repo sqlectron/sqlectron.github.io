4b50640103145552025b89f0b927cb6a8e8fc8af
Matthew Peveler <matt.peveler@gmail.com>
2020-08-04 16:06:45 -0400

    refactor website for webpack 4 (#8)
    The transition from webpack 1 -> 4 allows a number of changes
    to how the site was being built previously. Minimally, this involved
    removal of a number of deprecated / removed plugins, upgrading
    the syntax, upgrading all the loaders, reconfiguring the loaders as
    made sense (e.g. babel activates react-hot-loader automatically).
    
    As part of this effort, the dist/ folder has also been removed, in-favor
    of instead using a dedicated gh-pages branch, which will get updated via
    a GH action workflow, instead of requiring a manual push on the part
    of a developer. This simplifies things, as well as makes "drive-by"
    commits easier.
    
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- src/root.js
+++ src/root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ga from 'react-google-analytics';
+//import ga from 'react-google-analytics';
 
 import Header from './components/header.jsx';
 import Hero from './components/hero.jsx';
@@ -8,8 +8,9 @@ import AppInfoTerm from './components/app-info-term.jsx';
 import Contributor from './components/contributor.jsx';
 import Footer from './components/footer.jsx';
 
-const GAInitiailizer = ga.Initializer;
+//const GAInitiailizer = ga.Initializer;
 const isDev = process.env.NODE_ENV === 'development';
+
 if (typeof window !== 'undefined') {
   require('normalize.css');
   require('./main.scss');
@@ -18,49 +19,20 @@ if (typeof window !== 'undefined') {
 
 export default class Root extends React.Component {
   componentDidMount() {
-    ga('create', 'UA-70854359-1', 'auto');
-    ga('send', 'pageview');
+    //ga('create', 'UA-70854359-1', 'auto');
+    //ga('send', 'pageview');
   }
 
   render() {
     return (
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <title>Sqlectron - One single DB client for any relational DB</title>
-          <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
-          <meta property="twitter:card" content="summary"/>
-          <meta property="twitter:title" content="Sqlectron - One single DB client for any relational DB"/>
-          <meta property="twitter:description" content="A simple and lightweight SQL client desktop/terminal with cross database and platform support."/>
-          <meta property="twitter:image:src" content="https://sqlectron.github.io/logos/logo-square-180px.png"/>
-          <meta property="twitter:image:width" content="180"/>
-          <meta property="twitter:image:height" content="180"/>
-          <meta property="og:url" content="https://sqlectron.github.io"/>
-          <meta property="og:site_name" content="Sqlectron"/>
-          <meta property="og:title" content="Sqlectron - One single DB client for any relational DB"/>
-          <meta property="og:description" content="A simple and lightweight SQL client desktop/terminal with cross database and platform support."/>
-          <meta property="og:image" content="http://sqlectron.github.io/logos/logo-square-180px.png"/>
-          <meta property="og:image:secure_url" content="https://sqlectron.github.io/logos/logo-square-180px.png" />
-          <meta property="og:image:type" content="image/png"/>
-          <meta property="og:image:width" content="180"/>
-          <meta property="og:image:height" content="180"/>
-          {!isDev && <link rel="stylesheet" type="text/css" href="/dist/style.css"/>}
-        </head>
-        <body>
-          <div id="main">
-            <Header/>
-            <Hero/>
-            <AppInfoGUI/>
-            <AppInfoTerm/>
-            <Contributor/>
-            <Footer/>
-          </div>
-          {isDev && <script src="/hot.js"/>}
-          {!isDev && <script src="/dist/bundle.js"/>}
-          {!isDev && <GAInitiailizer />}
-        </body>
-      </html>
+      <div id="main">
+        <Header/>
+        <Hero/>
+        <AppInfoGUI/>
+        <AppInfoTerm/>
+        <Contributor/>
+        <Footer/>
+      </div>
     )
   }
 }
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const entry = {
+  bundle: './src/index.js',
+};
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(isDev ? 'development' : 'production'),
+  }),
+  new HtmlWebPackPlugin({
+    template: './src/index.html',
+  }),
+];
+
+if (isDev) {
+  entry.hot = [
+    'webpack-dev-server/client?http://localhost:4000',
+    'webpack/hot/only-dev-server',
+    './src/index.js',
+  ];
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+  );
+}
+else {
+  plugins.push(
+    new MiniCssExtractPlugin(),
+  );
+}
+
+const webpackConfig = {
+  mode: isDev ? 'development' : 'production',
+  entry: entry,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd'
+  },
+  plugins: plugins,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              esModule: false,
+              mimetype: 'image/png',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          }
+        ]
+      }
+    ]
+  }
+};
+
+if (isDev) {
+  webpackConfig.devServer = {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    port: 4000,
+    hot: true
+  };
+}
+
+module.exports = webpackConfig;
